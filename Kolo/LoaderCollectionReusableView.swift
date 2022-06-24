87845d34a8c5085040c794f9844f9b5d31c24bde
Intugine <techgill.harry@gmail.com>
2022-06-24 10:12:19 +0530

    image caching, loader, api caching using eTag

---
+++ Kolo/LoaderCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderCollectionReusableView.swift
+//  Kolo
+//
+//  Created by Intugine on 21/06/22.
+//
+
+import UIKit
+
+class LoaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
